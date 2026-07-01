af41177d2eff467c0bebcc86bcdb9268180b0353
Mawunyo Abiwu <theablemawunyo@gmail.com>
2026-07-01 09:14:02 +0000

    fixed pdf errors

--- App.py
+++ App.py
@@ -206,7 +206,7 @@ def generate_primary_pdf_report(primary_details, primary_final_net_take_home, pr
     primary_pdf.cell(200, 10, txt=f"Short Term Treasury Bill Target: GHS {primary_tbill:.2f}", ln=1)
     primary_pdf.cell(200, 10, txt=f"Final Net Take Home: GHS {primary_final_net_take_home:.2f}", ln=1)
 
-    return bytes(primary_pdf.output())
+    return primary_pdf.output().encode("latin")
 
 def generate_alternative_pdf_report(alternative_details, alternative_final_net_take_home, alternative_tbill):
     alternative_pdf = FPDF()
